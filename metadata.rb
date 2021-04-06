b699ae78b85a7af1d7538aaf41752055e4bd6dd5
Ryan <kennedy@osuosl.org>
2021-04-06 09:47:12 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache-2.0'
 description      'The OpenStack Advanced Volume Management service Cinder.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '18.0.0'
+chef_version '>= 16.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
@@ -20,4 +21,3 @@ depends 'selinux'
 
 issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
 source_url 'https://github.com/openstack/cookbook-openstack-block-storage' if respond_to?(:source_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
